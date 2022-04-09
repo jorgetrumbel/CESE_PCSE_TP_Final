a5572f721dc43f8b5dc841f621ae40f6d56d8cbf
Alejandro <56329158+jorgetrumbel@users.noreply.github.com>
2022-04-09 20:43:27 -0300

    Creacion del proyecto

---
+++ PCSE_ENCODER/PCSE_ENCODER/driver_init.c
@@ -0,0 +1,136 @@
+/*
+ * Code generated from Atmel Start.
+ *
+ * This file will be overwritten when reconfiguring your Atmel Start project.
+ * Please copy examples or other code you want to keep to a separate file
+ * to avoid losing it when reconfiguring.
+ */
+
+#include "driver_init.h"
+#include <peripheral_clk_config.h>
+#include <utils.h>
+#include <hal_init.h>
+
+/*! The buffer size for USART */
+#define USART_0_BUFFER_SIZE 16
+
+struct usart_async_descriptor USART_0;
+
+static uint8_t USART_0_buffer[USART_0_BUFFER_SIZE];
+
+void SPI_AS5047D_PORT_init(void)
+{
+
+	gpio_set_pin_level(PA08,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(PA08, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(PA08, PINMUX_PA08D_SERCOM2_PAD0);
+
+	gpio_set_pin_level(PA09,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   false);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(PA09, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(PA09, PINMUX_PA09D_SERCOM2_PAD1);
+
+	// Set pin direction to input
+	gpio_set_pin_direction(PA10, GPIO_DIRECTION_IN);
+
+	gpio_set_pin_pull_mode(PA10,
+	                       // <y> Pull configuration
+	                       // <id> pad_pull_config
+	                       // <GPIO_PULL_OFF"> Off
+	                       // <GPIO_PULL_UP"> Pull-up
+	                       // <GPIO_PULL_DOWN"> Pull-down
+	                       GPIO_PULL_OFF);
+
+	gpio_set_pin_function(PA10, PINMUX_PA10D_SERCOM2_PAD2);
+}
+
+void SPI_AS5047D_CLOCK_init(void)
+{
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM2_GCLK_ID_CORE, CONF_GCLK_SERCOM2_CORE_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM2_GCLK_ID_SLOW, CONF_GCLK_SERCOM2_SLOW_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	hri_mclk_set_APBCMASK_SERCOM2_bit(MCLK);
+}
+
+/**
+ * \brief USART Clock initialization function
+ *
+ * Enables register interface and peripheral clock
+ */
+void USART_0_CLOCK_init()
+{
+
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM3_GCLK_ID_CORE, CONF_GCLK_SERCOM3_CORE_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	hri_gclk_write_PCHCTRL_reg(GCLK, SERCOM3_GCLK_ID_SLOW, CONF_GCLK_SERCOM3_SLOW_SRC | (1 << GCLK_PCHCTRL_CHEN_Pos));
+	hri_mclk_set_APBCMASK_SERCOM3_bit(MCLK);
+}
+
+/**
+ * \brief USART pinmux initialization function
+ *
+ * Set each required pin to USART functionality
+ */
+void USART_0_PORT_init()
+{
+
+	gpio_set_pin_function(PA22, PINMUX_PA22C_SERCOM3_PAD0);
+
+	gpio_set_pin_function(PA23, PINMUX_PA23C_SERCOM3_PAD1);
+}
+
+/**
+ * \brief USART initialization function
+ *
+ * Enables USART peripheral, clocks and initializes USART driver
+ */
+void USART_0_init(void)
+{
+	USART_0_CLOCK_init();
+	usart_async_init(&USART_0, SERCOM3, USART_0_buffer, USART_0_BUFFER_SIZE, (void *)NULL);
+	USART_0_PORT_init();
+}
+
+void delay_driver_init(void)
+{
+	delay_init(SysTick);
+}
+
+void system_init(void)
+{
+	init_mcu();
+
+	// GPIO on PA11
+
+	gpio_set_pin_level(SPI_nCS_PIN,
+	                   // <y> Initial level
+	                   // <id> pad_initial_level
+	                   // <false"> Low
+	                   // <true"> High
+	                   true);
+
+	// Set pin direction to output
+	gpio_set_pin_direction(SPI_nCS_PIN, GPIO_DIRECTION_OUT);
+
+	gpio_set_pin_function(SPI_nCS_PIN, GPIO_PIN_FUNCTION_OFF);
+
+	SPI_AS5047D_CLOCK_init();
+	SPI_AS5047D_init();
+	SPI_AS5047D_PORT_init();
+	USART_0_init();
+
+	delay_driver_init();
+}
